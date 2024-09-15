e48724675238fe327db6466a54a846c6afeb8131
ShamrozSamad21 <shamrozas21@gmail.com>
2024-09-16 03:30:46 +0500

    Chp 1

---
+++ app.js
@@ -0,0 +1,12 @@
+console.log("hello world!")
+
+
+alert("Welcome Shamroz")
+
+
+alert("Error! Please enter a valid password")
+
+alert("Welcome to Js land... \n Happy coding!")
+
+alert("Welcome to Js Land")
+alert("Happy coding! \nprevent this page from creating additional dialogs")
